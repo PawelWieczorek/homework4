e51e7c96bd4ef33d34551c291564394fa2bc8400
pwieczorek <p.wieczoreknkse@gmail.com>
2016-09-10 12:46:38 +0200

    Initial commit

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by pwieczorek on 10.09.16.
+ */
+public class MainApplication {
+
+    private static String text = "ala ma kota i kot ma ale aLe KoTa nie widziano.";
+
+    public static void main(String[] args) {
+        //text.toLowerCase();
+        Pattern ala = Pattern.compile("al/S");
+        Pattern kot = Pattern.compile("kot");
+
+        Matcher mAla = ala.matcher(text);
+        Matcher mKot = kot.matcher(text);
+
+
+        boolean b = mAla.find();
+    }
+}
